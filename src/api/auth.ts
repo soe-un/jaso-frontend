cc666370b66d9b602d84b70b9e1364d19c0789ba
soe-un <runterf@gmail.com>
2025-03-23 20:54:56 +0900

    fix refresh... change jwt

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -1,6 +1,18 @@
 import { api } from "@/lib/api";
+import { User } from "@/store/auth";
 
 export const login = async (email: string, password: string) => {
   const res = await api.post("/auth/login", { email, password });
+  return res.data;
+};
+
+export const refresh = async (): Promise<{
+  user: User;
+  accessToken: string;
+}> => {
+  const res = await api.post("/auth/refresh", null, {
+    withCredentials: true,
+  });
+
   return res.data;
 };
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,17 @@
 "use client";
 
+import AutoLogin from "@/components/AutoLogin";
 import { useAuth } from "@/hooks/useAuth";
 
 export default function Home() {
   const { auth } = useAuth();
 
   return (
-    <div>{auth.isLoggedIn ? `${auth.user?.name}님 환영!` : "로그인 필요"}</div>
+    <>
+      <AutoLogin />
+      <main>
+        {auth.isLoggedIn ? `${auth.user?.name}님 환영!` : "로그인 필요"}
+      </main>
+    </>
   );
 }
